776e9b636527e3dd96d93d240182ca570f6af6b5
faiyazvaraliya31-droid <faiyazvaraliya31@gmail.com>
2026-01-20 10:54:36 +0530

    Fix formatting issues and update server port handling

--- student.py
+++ student.py
@@ -70,4 +70,5 @@ def delete_student():
         conn.close()
     return render_template("delete.html", message=message)
 if __name__ =="__main__":
-    app.run(debug=True)
+    port =int(os.environ.get("PORT",10000))
+    app.run(host='0.0.0.0' , port=port)
