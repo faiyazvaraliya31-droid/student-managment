296b94b4f42d5a8320a677edb565d32699e88868
faiyazvaraliya31-droid <faiyazvaraliya31@gmail.com>
2026-01-20 11:56:15 +0530

    Refactor database path and add index route
    Updated database path to use a temporary directory and added an index route.

--- student.py
+++ student.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask,request,render_template
 app=Flask(__name__,template_folder='.')
 import sqlite3
+DB_PATH = "/tmp/student.db
 def init_db():
-    conn=sqlite3.connect("student.db")
+    conn=sqlite3.connect(DB_PATH)
     c=conn.cursor()
     c.execute("""
     CREATE TABLE IF NOT EXISTS student
@@ -13,13 +15,17 @@ def init_db():
     conn.commit()
     conn.close()
 init_db()
+@app.route("/")
+def index():
+    return render_template("student.html")
+    
 @app.route("/add_student", methods=["GET","POST"])
 def add_student():
     message=""
     if request.method=="POST":
         name=request.form.get("student_name")
         marks=request.form.get("student_marks")
-        conn=sqlite3.connect("student.db")
+        conn=sqlite3.connect(DB_PATH)
         c=conn.cursor()
         c.execute("insert into student (name,marks)values(?,?)",(name,marks))
         conn.commit()
@@ -29,7 +35,7 @@ def add_student():
 
 @app.route("/viws_student", methods=["GET","POST"])
 def viws_student():
-    conn=sqlite3.connect("student.db")
+    conn=sqlite3.connect(DB_PATH)
     c=conn.cursor()
     c.execute("select name,marks from student")
     students_list=c.fetchall()
@@ -43,7 +49,7 @@ def update_student():
     if request.method=="POST":
         name=request.form.get("student_name")
         update_marks=request.form.get("update_marks")
-        conn=sqlite3.connect("student.db")
+        conn=sqlite3.connect(DB_PATH)
         c=conn.cursor()
         c.execute("update student set marks=? where name=?",(update_marks,name))
         if c.rowcount >0:
@@ -59,7 +65,7 @@ def delete_student():
     message=""
     if request.method=="POST":
         name=request.form.get("delete_student")
-        conn=sqlite3.connect("student.db")
+        conn=sqlite3.connect(DB_PATH)
         c=conn.cursor()
         c.execute("delete from student where name=?",(name,))
         if c.rowcount >0:
@@ -69,8 +75,8 @@ def delete_student():
         conn.commit()
         conn.close()
     return render_template("delete.html", message=message)
-import os
 if __name__ =="__main__":
     port =int(os.environ.get("PORT",10000))
     app.run(host='0.0.0.0' , port=port)
+
 
